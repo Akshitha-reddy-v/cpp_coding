3a085d625fc1dec49e3224d53ed074a85001a451
Akshitha-reddy-v <142597246+Akshitha-reddy-v@users.noreply.github.com>
2025-05-12 23:12:23 +0530

    Create hello_world.cpp

---
+++ Day_1/hello_world.cpp
@@ -0,0 +1,10 @@
+// printing Hello World! on the console - single line comment
+#include <iostream>    // including header iostream in directive
+int main() {
+  std::cout<<"Hello World!";   // prints the string to the console 
+  return 0;
+}
+/* return 0 indicates that
+   program executed successfully
+   - multi line comments
+*/
